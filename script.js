1a857ec10e66a2fce2be69c150242928fdbc08f8
Ryan Milvenan <ryanmilvenan@gmail.com>
2014-01-24 17:18:28 -0700

    Finish adding Backbone, incorporate no-web-security

--- script.js
+++ script.js
@@ -1,80 +1,144 @@
 (function ($) {
-	function Beer (name, likes) {
-    this.name = name;
-    this.likes = likes;
-  }
+  /*
+    Beer Model and View
+  */
+	var Beer = Backbone.Model.extend ({
+    initialize: function () {
+      this.on("change", function () {
 
-  function rebind () {
-    $(".stand-in-up").on("click", function () {
-      var like = $(this).closest('.beer-type').find('ul .likes');
-      var likeNum = like.data('likes');
-      likeNum++;
-      like.data('likes', likeNum);
-      like.find('.like-num').text(likeNum);
-    });
+      });
+    },
+    defaults: {
+      name: "",
+      likes: 0
+    },
+    decLikes: function () {
+      var likes = this.get('likes');
+      likes--;
+      this.set({likes:likes});
+    },
+    addLikes: function () {
+      var likes = this.get('likes');
+      likes++;
+      this.set({likes:likes});
+    }
+  });
 
-    $(".stand-in-down").on("click", function () {
-      var like = $(this).closest('.beer-type').find('ul .likes');
-      var likeNum = like.data('likes');
-      likeNum--;
-      like.data('likes', likeNum);
-      like.find('.like-num').text(likeNum);
-    });
-  }
+  var BeerView = Backbone.View.extend ({
+    tagName: 'li',
+    className:"beer-type",
+    template: _.template("<%= name %>" +
+                         "<img class='small-down' />" +
+                         "<img class='small-up' />" +
+                         "<ul><li class='likes'>Likes: <%= likes %>"),
+    initialize: function () {
+      this.model.on("change", this.render, this);
+    },
+    events: {
+      "click .small-down": "decLikes",
+      "click .small-up": "addLikes"
+    },
+    render: function () {
+      var attr = this.model.toJSON();
+      this.$el.html(this.template( attr ));
+      return this;
+    },
+    decLikes: function() {
+      this.model.decLikes();
+    },
+    addLikes: function() {
+      this.model.addLikes();
+    }
+  });
 
-  function addBeer (newBeer) {
-    var likeNum = newBeer.likes;
-    var upArrow = $("<img class='stand-in-up' />");
-		var downArrow = $("<img class='stand-in-down' />");
-		var newBeer = $("<li class='beer-type'>" + newBeer.name + "</li>");
-    newBeer.append(downArrow);
-    newBeer.append(upArrow);
-    var likes = $("<ul><li class='likes' data-likes='"
-                  + likeNum + "'>Likes: <span class='like-num'>"
-                  + likeNum + "</span></li></ul>");
-    newBeer.append(likes);
-    newBeer.appendTo($('#beer-list'));
-    rebind();
-  }
+  /*
+    Beers Collection and View
+  */
+  var Beers = Backbone.Collection.extend ({
+    model: Beer,
+    url: 'http://beer.fluentcloud.com/v1/beer/'
 
-  $(document).ready(function () {
-    $(".side-button").bind("click", "#add-beer", function () {
-      $('#add-beer-box').slideToggle();
-    });
+  });
 
-    $(".stand-in-up").on("click", function () {
-      var like = $(this).closest('.beer-type').find('ul .likes');
-      var likeNum = like.data('likes');
-      likeNum++;
-      like.data('likes', likeNum);
-      like.find('.like-num').text(likeNum);
-    });
+  var BeerList = Backbone.View.extend ({
+    el: $("#beer-list"),
+    initialize: function () {
+      this.collection.on("add", this.addOne, this);
+      this.collection.on("reset", this.addAll, this);
+    },
+    addOne: function ( beer ) {
+      var beerView = new BeerView({model:beer});
+      this.$el.append(beerView.render().el);
+    },
+    addAll: function () {
+      this.collection.forEach(this.addOne, this);
+    },
+    render: function () {
+      this.addAll();
+    }
 
-     $(".stand-in-down").on("click", function () {
-      var like = $(this).closest('.beer-type').find('ul .likes');
-      var likeNum = like.data('likes');
-      likeNum--;
-      like.data('likes', likeNum);
-      like.find('.like-num').text(likeNum);
-    });
+  });
 
-    $("#up").on("click", function () {
-      var beerName = $("#beer-input").val();
-      var likes = 1;
-      var newBeer = new Beer(beerName, likes);
-      addBeer (newBeer);
+  /*
+    UI Views w/ no model
+  */
+
+  var AddBeerButton = Backbone.View.extend ({
+    el: $('#add-beer'),
+    initialize: function () {
+      this.$el.on("click", function () {
+        $('#add-beer-box').slideToggle();
+      });
+    }
+  });
+
+  var GetBeerButton = Backbone.View.extend ({
+    el: $('#get-beers'),
+    events: {
+      "click": "getBeers"
+    },
+    getBeers: function () {
+      $(".info-text").remove();
+      this.collection.fetch();
+    }
+  });
+
+  var AddBeerBox = Backbone.View.extend ({
+    el: $('#add-beer-box'),
+    events: {
+      "click #up": "submitUp",
+      "click #down": "submitDown"
+    },
+    submitUp: function () {
+      var beerName;
+      if ($('#beer-input').val() !== "") {
+        beerName = $('#beer-input').val();
+        var beerLikes = 1;
+        var newBeer = new Beer({name:beerName, likes: beerLikes});
+        this.collection.add(newBeer);
+        $(".info-text").remove();
+        $('#add-beer-box').slideToggle();
+        $('#beer-input').val("");
+      }
+    },
+    submitDown: function () {
+      var beerName = $('#beer-input').val();
+      var beerLikes = -1;
+      var newBeer = new Beer({name:beerName, likes: beerLikes});
+      this.collection.add(newBeer);
+      $(".info-text").remove();
       $('#add-beer-box').slideToggle();
       $('#beer-input').val("");
-    });
+    }
+  });
 
-    $("#down").on("click", function () {
-      var beerName = $("#beer-input").val();
-      var likes = -1;
-      var newBeer = new Beer(beerName, likes);
-      addBeer (newBeer);
-      $('#add-beer-box').slideToggle();
-      $('#beer-input').val("");
-    });
+  //Beer Collection Instantiation
+  var beers = new Beers();
+  var beerList = new BeerList({collection: beers});
 
-	});
+  //UI Instantiation
+  var addBeerButton = new AddBeerButton();
+  var getBeerButton = new GetBeerButton({collection: beers});
+  var addBeerBox = new AddBeerBox({collection: beers});
+
 })(jQuery);
